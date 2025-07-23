89b981d0e56b019617d91181c8acff5b787956c3
dest411 <vadimstrizhak652@gmail.com>
2025-07-24 02:33:12 +0300

    if else jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import './main.css'
 
 const App = () => {
+    const name = 'Alex';
+    const age = 18;
+    let attr;
+
+    if (age === 18) {
+        attr = 'good';
+    }else{
+        attr = 'bad';
+    }
+
     return (
-        <div className="hello" >
-           <input type="text" />
-           <p>sdfsd</p>
+        <div>
+           <div className={attr} >
+                {name}
+           </div>
         </div>
         
     );
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
      
     <App/>
     <div className='hello' >
-      sdfsd
+      
     </div>
   </React.StrictMode>
 );
