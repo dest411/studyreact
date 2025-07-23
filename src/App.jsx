7fb1b2b662ceafd88e49978ff56d48cf5ae50c7b
dest411 <vadimstrizhak652@gmail.com>
2025-07-24 02:47:51 +0300

    jsx beginner

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,55 @@ import React from "react";
 import './main.css'
 
 const App = () => {
-    const name = 'Alex';
+    let name = 'Alex';
     const age = 18;
     let attr;
 
     if (age === 18) {
         attr = 'good';
+        name = 'Vadik';
     }else{
         attr = 'bad';
     }
 
+    function test1() {
+        if (age === 18) {
+            
+            return (
+            <div className="test1" >
+                dfgdgfd
+                dfgdgfdfg
+
+
+                dsfgsdg
+            </div>
+
+            );
+        }
+        else{
+            return (
+                <div className="test2" >
+                    sdfsdf
+                    sdsgsfd
+                    sdfsdfsd
+                </div>
+            )
+        }
+    }
+
     return (
         <div>
-           <div className={attr} >
+
+           {/* <div className={attr} >
                 {name}
-           </div>
+           </div> */}
+
+           {/* <div>
+                {age === 18 ? 'good' : 'bad'}
+           </div> */}
+            
+            {test1()}
+
         </div>
         
     );
