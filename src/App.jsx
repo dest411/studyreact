34a8b1d996407bb8ab4f0c51aa7fd6cd8a54be5e
dest411 <vadimstrizhak652@gmail.com>
2025-07-24 02:27:12 +0300

    jsx trainee

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './main.css'
+
+const App = () => {
+    return (
+        <div className="hello" >
+           <input type="text" />
+           <p>sdfsd</p>
+        </div>
+        
+    );
+};
+
+export default App;
