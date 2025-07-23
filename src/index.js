8bf14a749f9d49c26ba876912147eabedc62f2a3
dest411 <vadimstrizhak652@gmail.com>
2025-07-24 02:14:20 +0300

    jsx

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import fd , { TEST1, TEST2 } from './test';
-import Dog from './dog.jpg';
-import './index.css';
-import { hello } from './myfolder/hello';
-import { hello2 } from './myfolder';
-import App from './App';
 
-console.log(hello, hello2);
-
-console.log(fd);
-console.log(TEST1,TEST2);
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root =  ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App></App>
+    
     <div className='hello' >
-      <img src={Dog}/>
+      sdfsd
     </div> 
     
   </React.StrictMode>
--- src/test.js
+++
@@ -1,7 +0,0 @@
-export const TEST1 = 5;
-export const TEST2 = 6;
-
-
-const TEST5 = 7;
-
-export default TEST5;
