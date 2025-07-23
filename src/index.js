8e575c7b88504b0d3c82ad231d947b6f85a9af7d
dest411 <vadimstrizhak652@gmail.com>
2025-07-23 15:23:35 +0300

    import/export completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import fd , { TEST1, TEST2 } from './test';
+import Dog from './dog.jpg';
+import './index.css';
+import { hello } from './myfolder/hello';
+import { hello2 } from './myfolder';
+import App from './App';
+
+console.log(hello, hello2);
+
 console.log(fd);
 console.log(TEST1,TEST2);
 
@@ -10,7 +18,20 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    dfgfdgf
+    <App></App>
+    <div className='hello' >
+      <img src={Dog}/>
+    </div> 
+    
   </React.StrictMode>
 );
  
+// import / export
+
+// export const TEST = ....L
+// export { TEST1, testFunction }
+// > import { TEST } from './...';
+
+
+// export default TEST;
+// > import SFSDGSDGS from './....';
