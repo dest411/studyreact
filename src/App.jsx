5e6883a5352bc322e1ad5615d1d4a47d1dec4158
dest411 <vadimstrizhak652@gmail.com>
2025-08-08 11:50:49 +0300

    comm

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,13 @@
 import React from 'react';
 import './main.css'
-import UserName from './UserName';
 
-let USERS = [
-    { id: 1, name: 'Alex', surname: 'test', age: 20, passportId: 123 },
-    { id: 2, name: 'Alex', surname: 'test2', age: 20, passportId: 66 },
-    { id: 3, name: 'John', surname: 'test3', age: 56, passportId: 123 },
-    { id: 4, name: 'Bob', surname: 'test4', age: 10, passportId: 50 },
-];
 
 const App = () => {
-    const chanegeName = (id, newName) => {
-        USERS = USERS.map((user) => {
-            if (user.id === id) {
-                return {
-                    id: user.id,
-                    name: newName,
-                    surname: user.surname,
-                    age: user.age,
-                    passportId: user.passportId
-                }
-            }
-            return user;
-        })
-    };
-    const data = [];
-    USERS.forEach((user) => {
-        const key = generateKey(user);
-        data.push(
-            <div key={key} >
-                <UserName 
-                    userName = {user.name}
-                    userSurname = {user.surname}
-                    userAge = {user.age}
-                    chanegeName = {chanegeName}
-                 />
-            </div>
-        );
-    } )
-
     return (
         <div>
-           {data}
+           test
         </div>
     )
 };
-function generateKey(user) {
-    return `${user.name}_${user.surname}_${user.age}_${user.passportId}`;
-}
 
 export default App;
