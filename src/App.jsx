b32a06e36db84701d228276c1713c8ab22708738
dest411 <vadimstrizhak652@gmail.com>
2025-08-08 14:07:34 +0300

    14 lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,52 @@
 import React from 'react';
 import './main.css'
+import UserName from './UserName';
 
+let USERS = [
+    { id: 1, name: 'Alex', surname: 'test', age: 20, passportId: 123 },
+    { id: 2, name: 'Alex', surname: 'test2', age: 20, passportId: 66 },
+    { id: 3, name: 'John', surname: 'test3', age: 56, passportId: 123 },
+    { id: 4, name: 'Bob', surname: 'test4', age: 10, passportId: 50 },
+];
 
 const App = () => {
-
-    const now = new Date();
-
-    const handleClick = (event) => {
-        console.log('Hello',event);
-        
+    const chanegeName = (id, newName) => {
+        USERS = USERS.map((user) => {
+            if (user.id === id) {
+                return {
+                    id: user.id,
+                    name: newName,
+                    surname: user.surname,
+                    age: user.age,
+                    passportId: user.passportId
+                }
+            }
+            return user;
+        })
     };
-    
-    const onChangeHandler = (event) =>{
-        console.log('Input changed', event.target.value);
-        
-    }
+    const data = [];
+    USERS.forEach((user) => {
+        const key = generateKey(user);
+        data.push(
+            <div key={key} >
+                <UserName 
+                    userName = {user.name}
+                    userSurname = {user.surname}
+                    userAge = {user.age}
+                    chanegeName = {chanegeName}
+                 />
+            </div>
+        );
+    } )
 
     return (
-        <div className='test'>
-            <input type="text" onChange={onChangeHandler} />
-            <button className='btn' onClick={handleClick}>Hello</button>
-            <h1>Current Date: {now.toLocaleTimeString()}</h1>
+        <div>
+           {data}
         </div>
-        
-    );
+    )
 };
+function generateKey(user) {
+    return `${user.name}_${user.surname}_${user.age}_${user.passportId}`;
+}
 
 export default App;
