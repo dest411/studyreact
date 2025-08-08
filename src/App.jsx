06a1f9a0dc6d311ccdcbda381208de83e7118799
dest411 <vadimstrizhak652@gmail.com>
2025-08-08 12:59:59 +0300

    com

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import './main.css'
 
 const App = () => {
 
+    const now = new Date();
+
     const handleClick = (event) => {
         console.log('Hello',event);
         
     };
+    
+    const onChangeHandler = (event) =>{
+        console.log('Input changed', event.target.value);
+        
+    }
 
     return (
         <div className='test'>
-            <input type="text" onChange={(event) =>{
-                console.log('Input changed',event.target.value);
-            }} />
+            <input type="text" onChange={onChangeHandler} />
             <button className='btn' onClick={handleClick}>Hello</button>
+            <h1>Current Date: {now.toLocaleTimeString()}</h1>
         </div>
+        
     );
 };
 
