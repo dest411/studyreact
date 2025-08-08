21341302be94cc1223dfb9d64ad4431a6956ce91
dest411 <vadimstrizhak652@gmail.com>
2025-08-08 12:31:15 +0300

    onChange

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import './main.css'
 
 
 const App = () => {
+
+    const handleClick = (event) => {
+        console.log('Hello',event);
+        
+    };
+
     return (
-        <div>
-           test
+        <div className='test'>
+            <input type="text" onChange={(event) =>{
+                console.log('Input changed',event.target.value);
+            }} />
+            <button className='btn' onClick={handleClick}>Hello</button>
         </div>
-    )
+    );
 };
 
 export default App;
