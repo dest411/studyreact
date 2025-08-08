94f5cb383c38cccbb6bef94190a6adee57dcb13a
dest411 <vadimstrizhak652@gmail.com>
2025-08-08 15:23:27 +0300

    movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,92 @@
-import React from 'react';
-import './main.css'
-import UserName from './UserName';
+// import React from 'react';
+// import './main.css'
+// import UserName from './UserName';
 
-let USERS = [
-    { id: 1, name: 'Alex', surname: 'test', age: 20, passportId: 123 },
-    { id: 2, name: 'Alex', surname: 'test2', age: 20, passportId: 66 },
-    { id: 3, name: 'John', surname: 'test3', age: 56, passportId: 123 },
-    { id: 4, name: 'Bob', surname: 'test4', age: 10, passportId: 50 },
+// let USERS = [
+//     { id: 1, name: 'Alex', surname: 'test', age: 20, passportId: 123 },
+//     { id: 2, name: 'Alex', surname: 'test2', age: 20, passportId: 66 },
+//     { id: 3, name: 'John', surname: 'test3', age: 56, passportId: 123 },
+//     { id: 4, name: 'Bob', surname: 'test4', age: 10, passportId: 50 },
+// ];
+
+// const App = () => {
+//     const [_, forceUpdate] = React.useReducer(x => x + 1, 0);
+//     const chanegeName = (id, newName) => {
+//         return () => {
+//             console.log(id, newName);
+//             USERS = USERS.map((user) => {
+//                 if (user.id === id) {
+//                     return {
+//                         id: user.id,
+//                         name: newName,
+//                         surname: user.surname,
+//                         age: user.age,
+//                         passportId: user.passportId
+//                     };
+//                 }
+//                 return user;
+//             });
+//             console.log(USERS);
+//             forceUpdate(); 
+//         };
+//     };
+//     const data = [];
+//     USERS.forEach((user) => {
+//         const key = generateKey(user);
+//         data.push(
+//             <div key={key} >
+//                 <UserName
+//                     userId = {user.id}
+//                     userName = {user.name}
+//                     userSurname = {user.surname}
+//                     userAge = {user.age}
+//                     chanegeName = {chanegeName}
+//                  />
+//             </div>
+//         );
+//     } )
+
+//     return (
+//         <div>
+//            {data}
+//         </div>
+//     )
+// };
+// function generateKey(user) {
+//     return `${user.name}_${user.surname}_${user.age}_${user.passportId}`;
+// }
+
+// export default App;
+
+import React from 'react';
+
+const MOVIES = [
+    {
+        id: 1,
+        name: 'Про мужика',
+        description: 'Про какого-то мужика, который что-то сделал',
+        img: 'https://festagent.com/system/tilda/tild6137-6632-4661-a361-613336646361__e4fac156b38cab487ec4.jpg'
+    },
+    {
+        id: 2,
+        name: 'Про мужика с медведем',
+        description: 'Медведь читает газету',
+        author: { name: 'John' },
+        img: 'https://www.film.ru/sites/default/files/images/crobn_013h_g_spa-ar_70x100_002_nuevo.jpg' 
+    },
+    {
+        id: 3,
+        name: 'Про мужика-акулу',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLMW66ta222_snvsmRO9GlNgMz2oi9DtDYnT2HpvDFeSrvLANwYu1xjzeS-OjV-MKu3Pk&usqp=CAU'
+    },
 ];
 
 const App = () => {
-    const chanegeName = (id, newName) => {
-        USERS = USERS.map((user) => {
-            if (user.id === id) {
-                return {
-                    id: user.id,
-                    name: newName,
-                    surname: user.surname,
-                    age: user.age,
-                    passportId: user.passportId
-                }
-            }
-            return user;
-        })
-    };
-    const data = [];
-    USERS.forEach((user) => {
-        const key = generateKey(user);
-        data.push(
-            <div key={key} >
-                <UserName
-                    userId = {user.id}
-                    userName = {user.name}
-                    userSurname = {user.surname}
-                    userAge = {user.age}
-                    chanegeName = {chanegeName}
-                 />
-            </div>
-        );
-    } )
-
     return (
         <div>
-           {data}
+            {MOVIES}
         </div>
     )
-};
-function generateKey(user) {
-    return `${user.name}_${user.surname}_${user.age}_${user.passportId}`;
 }
 
 export default App;
--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+// import React from 'react';
 
-const UserName = (props) => {
-    console.log(props);
+// const UserName = (props) => {
+//     const onClick = () => {
+//         props.chanegeName(props.userId, 'Admin')();
+//     };
+//     console.log(props);
     
-    return (
-        props.userAge > 20 ? (
-            <p className='dsfsdfs'> 
-                {props.userName}
-                <button  >Змінити ім'я</button>            
-            </p>
-        ): (
-            <p className='safsd' >{props.userName} {props.userSurname}</p>
-        )
-    );
-};
-export default UserName;
+//     return (
+//         props.userAge > 20 ? (
+//             <p className='dsfsdfs'> 
+//                 {props.userName}
+//                 <button onClick={onClick} >Змінити ім'я</button>            
+//             </p>
+//         ): (
+//             <p className='safsd' >{props.userName} {props.userSurname}</p>
+//         )
+//     );
+// };
+// export default UserName;
