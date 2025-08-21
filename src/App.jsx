5c0112b4150eaee451bae82f0116ada6d3b2a278
dest411 <vadimstrizhak652@gmail.com>
2025-08-21 15:29:27 +0300

    tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,33 @@ import './main.css'
 
 const App = () => {
     
+    const [task, setTask] = useState('');
+    const [tasksList, setTasksList] = useState([]);
+
+
+    const handeChange = (event) => {
+        setTask(event.target.value);
+    }
+
+    const addTask = () =>{
+        if (task.trim() !== ''){
+            setTasksList([...tasksList, task])
+            setTask('');
+        } else{
+            console.log('Please enter a task');
+        }
+    }
 
     return (
         <div>
             <Header/>
+            <input onChange={handeChange} value={task} type="text"/>
+            <button onClick={addTask}>Add</button>
+            <ul>
+                {tasksList.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
         </div>
     )
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
     return (
         <header>
             <h1>My React App</h1>
+            <h3>TO DO LIST</h3>
         </header>
     );
 }
