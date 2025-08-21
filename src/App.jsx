c42da6437b7bca15152c2df4eb294833b549c752
dest411 <vadimstrizhak652@gmail.com>
2025-08-21 14:49:55 +0300

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
-import React, { useState } from 'react';
-import Children from './Children';
+import React from 'react';
 import './main.css'
 
 const App = () => {
-    const [state, setState] = useState(false);
-    const [count, setCount] = useState(0);
-
-    const onClickHandler = () => {
-        setState(!state);
-    }
-
-    const countHandler = () =>{
-        setCount(count + 1);
-    }
-    const countHandlerr = () =>{
-        setCount(count - 1);
-    }
-    
     return (
         <div>
-            <button onClick={countHandler} >+</button>
-            <button onClick={countHandlerr} >-</button>
-            <button onClick={onClickHandler} >switch</button>
-            {state ? <Children count={count} /> : null}
+            
         </div>
     )
 
