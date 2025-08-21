3cc6ab188dc7d291aa197698c4769e5fe5cfcb78
dest411 <vadimstrizhak652@gmail.com>
2025-08-21 15:05:11 +0300

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import React, {useState} from 'react';
+import Header from './Header';
 import './main.css'
 
 const App = () => {
-
-    const [count, setCount] = useState(0);
-
-    const plusCount = () =>{
-        setCount(count +1);
-    }
-    const minusCount = () =>{
-        setCount(count -1);
-    }
-    const reset = () => {
-        setCount(0);
-    }
+    
 
     return (
         <div>
-            <h1>Count: {count}</h1>
-            <button onClick={plusCount}>+</button>
-            <button onClick={minusCount}>-</button>
-            <button onClick={reset} >reset</button>
+            <Header/>
         </div>
     )
 
