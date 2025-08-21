9b07def8872dfc311160ab6270672b563f72cdd0
dest411 <vadimstrizhak652@gmail.com>
2025-08-21 14:57:45 +0300

    comm

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './main.css'
 
 const App = () => {
+
+    const [count, setCount] = useState(0);
+
+    const plusCount = () =>{
+        setCount(count +1);
+    }
+    const minusCount = () =>{
+        setCount(count -1);
+    }
+    const reset = () => {
+        setCount(0);
+    }
+
     return (
         <div>
-            
+            <h1>Count: {count}</h1>
+            <button onClick={plusCount}>+</button>
+            <button onClick={minusCount}>-</button>
+            <button onClick={reset} >reset</button>
         </div>
     )
 
